0411170201d68b18c1195c1a5d89b170d3f6b6a7
Davronbek Normamatov <davronnormamatov004@gmail.com>
2024-10-19 15:07:26 +0500

    8th commit

--- src/main/java/com/igriss/ListIn/service/EmailService.java
+++ src/main/java/com/igriss/ListIn/service/EmailService.java
@@ -4,10 +4,7 @@ import com.igriss.ListIn.dto.security_dto.EmailVerificationRequestDTO;
 import com.igriss.ListIn.exceptions.EmailNotFoundException;
 import com.igriss.ListIn.exceptions.UserHasAccountException;
 
-import java.io.IOException;
-
 public interface EmailService {
     String verifyEmail(EmailVerificationRequestDTO verificationRequestDTO, String code) throws UserHasAccountException;
     EmailVerificationRequestDTO sendEmail(EmailVerificationRequestDTO verificationRequestDTO)  throws EmailNotFoundException;
-
 }
