ef358c7bb495ea63a564e8cff924e1900531f14c
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2024-11-06 20:08:34 +0500

    .env privacy file added

--- src/main/java/com/igriss/ListIn/ListInApplication.java
+++ src/main/java/com/igriss/ListIn/ListInApplication.java
@@ -1,5 +1,6 @@
 package com.igriss.ListIn;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,6 +12,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class ListInApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(ListInApplication.class, args);
 	}
 
