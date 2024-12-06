061a35a9d517cc325c902f9db950469adab4e402
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2024-12-06 22:24:48 +0500

    pc brand models inserted

---
+++ src/main/java/com/igriss/ListIn/publication/entity/PCBrandModel.java
@@ -0,0 +1,29 @@
+package com.igriss.ListIn.publication.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "pc_brand_models")
+public class PCBrandModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "pc_brand_model_id")
+    private UUID id;
+
+    @Column(nullable = false)
+    private String value;
+
+    @ManyToOne
+    @JoinColumn(name = "attribute_value_id")
+    private AttributeValue attributeKey;
+}
---
+++ src/main/resources/database_sql_scripts/models/pc_brand_models.sql
@@ -0,0 +1,141 @@
+----Acer models
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('9278faf8-0cdf-4eb5-8ff8-8736e9700e77','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Predator Helios 16');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('fcffec8e-e502-4bd0-aa86-3beba4200e1a','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Predator Helios 18');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('5013d58f-39f3-4980-930a-bec7a676cdfc','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Predator Triton 500 SE');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('40ba09f6-2c59-47f8-90ee-187f18909c4e','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Predator Triton 500');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('07da837d-3b07-4510-bf9b-265a7800cec4','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Predator Orion 3000');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('e1e06c35-193e-4084-b7d2-06ca90b5cf5a','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Predator Orion 5000');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('bec681f9-185e-4942-a678-b283d5c2fe37','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Predator X');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('0856307f-49e0-4d10-a9f4-62de40f2cd5f','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Aspire 5');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('d36c5b04-15bf-4ef9-8ab7-6e091a4a0970','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Aspire 3');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('f576127b-5c37-429d-a3a3-1b2097617090','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Aspire 1');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('2bd42d0f-e98f-468d-9e78-19b719d7e387','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Aspire C');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('3a336177-5eac-4b9c-bb32-844b905fe333','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Aspire Z');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('e1a2f85e-90a5-4d5c-b334-39b9e2c44865','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Swift 3');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('e6a371c6-c9a7-4f1b-9b57-84a97291e18a','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Swift 5');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('904759a6-be8f-4e5c-a13c-bb7bce7a89dd','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Swift 7');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('711bff85-0bc8-44a4-9322-976ed4a2fbae','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Spin 5');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('a19c8b0f-2d02-4315-8910-7f125b24cdc2','d4e36331-9c15-48e6-b7be-6abe3259ac0d','Spin 3');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('5bc83f93-e6cd-40a4-ad56-2adfa08db366','d4e36331-9c15-48e6-b7be-6abe3259ac0d','ConceptD 7');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('4b2ba9a7-99de-4b54-8f22-236d56fbba75','d4e36331-9c15-48e6-b7be-6abe3259ac0d','ConceptD 5');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('f950646c-05aa-4601-bf7b-3a97770b6112','d4e36331-9c15-48e6-b7be-6abe3259ac0d','ConceptD 3');
+
+----Apple models
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('2e7cb39a-5862-4e98-873e-de7d3575f076','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 24-inch (2021)');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('c9258573-5668-458d-8d5b-de0467d95abf','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 27-inch (2020)');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('3c5e2936-d32f-4a43-b203-d2a2dbbe3ad0','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac Pro');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('f6b69120-a335-470c-9653-a31f06316d17','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 21.5-inch (2019)');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('713ddb0f-2f25-4ffb-a434-292b2fee354b','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 27-inch (2019)');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('1ba835e6-3501-48cc-b281-e007f501141b','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 21.5-inch (2017)');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('0a046dfc-4543-4099-a78a-5808680698ff','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 27-inch (2017)');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('c6b51ede-8f76-4aec-9877-1086acb93fa2','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 21.5-inch (2015)');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('072cf6b1-53c2-4163-b56d-74f5029c8871','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 27-inch (2015)');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('fb948f43-c536-4ba4-86bf-07a6e2a991dd','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 21.5-inch (2013)');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('f3192a30-a0c9-466d-b92d-2e65a055cabf','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 27-inch (2013)');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('0557e4a5-0447-488f-994b-faf96ee08641','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 21.5-inch (2011)');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('771fdc9f-453e-41ea-8f60-c0161b8d2272','c4f83b0f-c98e-4ebe-aec3-0c718779aba9','iMac 27-inch (2011)');
+
+----Asus models
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('7a3c354e-0b98-4829-9a05-f888ed8fe0d9','4ae62814-8e47-495b-a524-5234f21a446c','ROG Strix GA35');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('8cb690a6-a60d-4028-84ed-2e2344325037','4ae62814-8e47-495b-a524-5234f21a446c','ROG Strix GT35');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('ee9a72e6-fab6-4d39-a4dc-a1ace198f163','4ae62814-8e47-495b-a524-5234f21a446c','ROG Strix GT15');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('43470eaf-52ab-4c8f-9af8-5c233c4448ca','4ae62814-8e47-495b-a524-5234f21a446c','TUF Gaming GT301');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('c72162cf-9e7e-40bf-9679-2390e83e6521','4ae62814-8e47-495b-a524-5234f21a446c','TUF Gaming GT501');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('183333e3-cd65-4e2a-a23f-157891ce27b9','4ae62814-8e47-495b-a524-5234f21a446c','VivoPC X');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('0c15c58d-32ee-465c-b3cb-1b415dc870d3','4ae62814-8e47-495b-a524-5234f21a446c','VivoPC');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('2010bba3-a62c-4ece-9c3f-7489261a9e42','4ae62814-8e47-495b-a524-5234f21a446c','ExpertCenter D5');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('55441e23-e632-4e5b-bdc7-4a7d13d3038a','4ae62814-8e47-495b-a524-5234f21a446c','ExpertCenter D3');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('17b5cd25-5902-47f9-a02b-cef033ae7f30','4ae62814-8e47-495b-a524-5234f21a446c','ASUS Mini PC PN50');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('f26dff30-df4a-486f-a40c-19c00e836cc9','4ae62814-8e47-495b-a524-5234f21a446c','ASUS Mini PC PN62');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('645286c7-13cf-4ac3-9c37-fa7ff5ed6a5b','4ae62814-8e47-495b-a524-5234f21a446c','ASUS ProArt Station D940MX');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('af44318a-339c-4637-8a0b-e64906a98b7a','4ae62814-8e47-495b-a524-5234f21a446c','ASUS ProArt Station PA90');
+
+----Dell models
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('7a3c354e-0b98-4829-9a05-f888ed8fe0d9','f0ccbe1f-9630-41a9-b16b-efdb5488826a','XPS Desktop');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('8cb690a6-a60d-4028-84ed-2e2344325037','f0ccbe1f-9630-41a9-b16b-efdb5488826a','Alienware Aurora R13');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('ee9a72e6-fab6-4d39-a4dc-a1ace198f163','f0ccbe1f-9630-41a9-b16b-efdb5488826a','Alienware Aurora R12');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('43470eaf-52ab-4c8f-9af8-5c233c4448ca','f0ccbe1f-9630-41a9-b16b-efdb5488826a','Alienware Aurora R11');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('c72162cf-9e7e-40bf-9679-2390e83e6521','f0ccbe1f-9630-41a9-b16b-efdb5488826a','Inspiron 3880');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('183333e3-cd65-4e2a-a23f-157891ce27b9','f0ccbe1f-9630-41a9-b16b-efdb5488826a','Inspiron 3881');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('0c15c58d-32ee-465c-b3cb-1b415dc870d3','f0ccbe1f-9630-41a9-b16b-efdb5488826a','Inspiron 3670');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('2010bba3-a62c-4ece-9c3f-7489261a9e42','f0ccbe1f-9630-41a9-b16b-efdb5488826a','Inspiron 5675');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('55441e23-e632-4e5b-bdc7-4a7d13d3038a','f0ccbe1f-9630-41a9-b16b-efdb5488826a','Vostro 3670');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('17b5cd25-5902-47f9-a02b-cef033ae7f30','f0ccbe1f-9630-41a9-b16b-efdb5488826a','Vostro 3681');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('f26dff30-df4a-486f-a40c-19c00e836cc9','f0ccbe1f-9630-41a9-b16b-efdb5488826a','OptiPlex 7090');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('645286c7-13cf-4ac3-9c37-fa7ff5ed6a5b','f0ccbe1f-9630-41a9-b16b-efdb5488826a','OptiPlex 7070');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('af44318a-339c-4637-8a0b-e64906a98b7a','f0ccbe1f-9630-41a9-b16b-efdb5488826a','OptiPlex 5050');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('f26dff30-df4a-486f-a40c-19c00e836cc9','f0ccbe1f-9630-41a9-b16b-efdb5488826a','OptiPlex 3060');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('645286c7-13cf-4ac3-9c37-fa7ff5ed6a5b','f0ccbe1f-9630-41a9-b16b-efdb5488826a','Precision 5820 Tower');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('af44318a-339c-4637-8a0b-e64906a98b7a','f0ccbe1f-9630-41a9-b16b-efdb5488826a','Precision 5820 Rack');
+
+----HP models
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('7a3c354e-0b98-4829-9a05-f888ed8fe0d9','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP Pavilion Desktop');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('8cb690a6-a60d-4028-84ed-2e2344325037','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP Pavilion All-in-One');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('ee9a72e6-fab6-4d39-a4dc-a1ace198f163','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP Envy Desktop');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('43470eaf-52ab-4c8f-9af8-5c233c4448ca','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP Envy All-in-One');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('c72162cf-9e7e-40bf-9679-2390e83e6521','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP EliteDesk 800 G6');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('183333e3-cd65-4e2a-a23f-157891ce27b9','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP EliteOne 800 G6');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('0c15c58d-32ee-465c-b3cb-1b415dc870d3','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP ProDesk 400 G7');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('2010bba3-a62c-4ece-9c3f-7489261a9e42','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP ProOne 400 G6');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('55441e23-e632-4e5b-bdc7-4a7d13d3038a','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP Omen 30L');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('17b5cd25-5902-47f9-a02b-cef033ae7f30','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP Omen 25L');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('f26dff30-df4a-486f-a40c-19c00e836cc9','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP Chromebox');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('645286c7-13cf-4ac3-9c37-fa7ff5ed6a5b','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP Slim Desktop');
+INSERT INTO pc_brand_models (pc_brand_model_id, attribute_value_id, value ) VALUES ('af44318a-339c-4637-8a0b-e64906a98b7a','291c3fd7-26c0-4c99-ad39-e6962bd2dd01','HP 290 G2');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
