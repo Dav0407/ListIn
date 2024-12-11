d04f44f77f073d75c3918e637367c796cf064e85
Davronbek Normamatov <davronnormamatov004@gmail.com>
2024-12-11 10:18:58 +0500

    small changes

---
+++ src/main/java/com/igriss/ListIn/publication/controller/CategoryTreeController.java
@@ -0,0 +1,25 @@
+package com.igriss.ListIn.publication.controller;
+
+import com.igriss.ListIn.publication.dto.category_tree.ParentNode;
+import com.igriss.ListIn.publication.service.CategoryTreeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/category-tree")
+@RequiredArgsConstructor
+public class CategoryTreeController {
+
+    private final CategoryTreeService categoryTreeService;
+
+    @GetMapping()
+    public ResponseEntity<List<ParentNode>> getCategoryTree() {
+        return ResponseEntity.ok(categoryTreeService.getCategoryTree());
+    }
+}
