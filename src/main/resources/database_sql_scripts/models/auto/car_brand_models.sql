fc0a2db2dbabf5cada607c2e41592928ff5f640b
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2025-02-05 19:46:35 +0500

    attribute values for cars

---
+++ src/main/resources/database_sql_scripts/models/auto/car_brand_models.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('186c5cbe-e64f-4619-a9e9-2b1cf760f933','ab07c3c9-5345-4e07-8132-fe71ee6e7a3a', '82e816c1-644c-458d-bc53-2ff99284172c','PlayStation 5', 'PlayStation 5', 'PlayStation 5');
