ba735ac830dcd205c5a6a2cfb7293ba7f40a95b3
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2025-02-05 19:46:35 +0500

    attribute values for cars

--- src/main/java/com/igriss/ListIn/user/controller/UserController.java
+++ src/main/java/com/igriss/ListIn/user/controller/UserController.java
@@ -10,7 +10,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.apache.coyote.BadRequestException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
