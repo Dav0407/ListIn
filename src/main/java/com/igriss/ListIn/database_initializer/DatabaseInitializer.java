fc60e967d5f867b7da8b07667e9b169dd24a5efc
Davronbek Normamatov <davronnormamatov004@gmail.com>
2025-02-06 22:15:50 +0500

    Changes by abdulaxad

--- src/main/java/com/igriss/ListIn/database_initializer/DatabaseInitializer.java
+++ src/main/java/com/igriss/ListIn/database_initializer/DatabaseInitializer.java
@@ -59,7 +59,7 @@ public class DatabaseInitializer {
             "/database_sql_scripts/numerics/auto_numeric_fields.sql"
     );
 
-    @PostConstruct
+/*    @PostConstruct
     public void flushRedis() {
         Objects.requireNonNull(redisTemplate
                         .getConnectionFactory()
@@ -68,7 +68,7 @@ public class DatabaseInitializer {
                 .serverCommands()
                 .flushAll();
         log.info("#Redis cache successfully cleared");
-    }
+    }*/
 
     @PostConstruct
     public void init() {
@@ -126,7 +126,7 @@ public class DatabaseInitializer {
     }
 
 
-    @PostConstruct
+/*    @PostConstruct
     public void clearElasticsearchData() {
         try {
             if (elasticsearchClient.indices().exists(e -> e.index(indexName)).value()) {
@@ -136,7 +136,7 @@ public class DatabaseInitializer {
         } catch (Exception e) {
             log.error("#Exception while clearing elastic search data: {}", e.getMessage());
         }
-    }
+    }*/
 }
 
 
