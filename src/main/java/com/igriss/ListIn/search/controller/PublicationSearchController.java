cc2a067f8cb58a2db4eb5f65ad5a37f6503c2149
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2025-02-08 22:33:23 +0500

    ...

--- src/main/java/com/igriss/ListIn/search/controller/PublicationSearchController.java
+++ src/main/java/com/igriss/ListIn/search/controller/PublicationSearchController.java
@@ -22,7 +22,8 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/publications/search")
 @RequiredArgsConstructor
-public class PublicationSearchController {
+public class
+PublicationSearchController {
 
 
     private final PublicationSearchService searchService;
--- src/main/resources/database_sql_scripts/models/auto/car_brand_models.sql
+++ src/main/resources/database_sql_scripts/models/auto/car_brand_models.sql
@@ -1,2 +1,84 @@
 
-INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('186c5cbe-e64f-4619-a9e9-2b1cf760f933','ab07c3c9-5345-4e07-8132-fe71ee6e7a3a', '82e816c1-644c-458d-bc53-2ff99284172c','PlayStation 5', 'PlayStation 5', 'PlayStation 5');
+--Abarth brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('186c5cbe-e64f-4619-a9e9-2b1cf760f933','694ad67c-1e9a-43ee-ab46-8ea6adcdc4a4', '49405788-6d5a-4736-b30a-59f77e6ed121','600e', '600e', '600e');
+
+
+
+--AC brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'f90995c5-2f73-43d2-ac0b-69bca0339343', '49405788-6d5a-4736-b30a-59f77e6ed121','', 'AC', '');
+
+
+
+--Acura brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'fc5e98dd-a9ca-4bfd-ac6b-427c1f660746', '49405788-6d5a-4736-b30a-59f77e6ed121','Acura', 'Acura', 'Acura');
+
+
+
+--Adler brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '25093c78-b489-41d4-9834-2757ce1ce8b8', '49405788-6d5a-4736-b30a-59f77e6ed121','Adler', 'Adler', 'Adler');
+
+
+
+--AITO brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '72b3ec88-6461-4804-a863-ff436f60845e', '49405788-6d5a-4736-b30a-59f77e6ed121','AITO', 'AITO', 'AITO');
+
+
+
+--Alfa Romeo brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '6970b4d0-99ae-48f0-9e75-b045e1f4b100', '49405788-6d5a-4736-b30a-59f77e6ed121','Alfa Romeo', 'Alfa Romeo', 'Alfa Romeo');
+
+
+
+--Alpina brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'fbf1a4b2-38a6-4674-9383-61a367dc9c81', '49405788-6d5a-4736-b30a-59f77e6ed121','Alpina', 'Alpina', 'Alpina');
+
+
+
+--Alpine brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'a02a8a6b-a85b-4f21-b28c-b384a2f54767', '49405788-6d5a-4736-b30a-59f77e6ed121','Alpine', 'Alpine', 'Alpine');
+
+
+
+--AMC brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'c8898280-f594-4cf2-bdde-5f62901c1d5d', '49405788-6d5a-4736-b30a-59f77e6ed121','AMC', 'AMC', 'AMC');
+
+
+
+--Amphicar brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '5bfe1c54-c192-4530-b8f1-d6b491957716', '49405788-6d5a-4736-b30a-59f77e6ed121','Amphicar', 'Amphicar', 'Amphicar');
+
+
+
+--Arcfox brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '0d7b8c23-e8ae-4af6-a70b-b2be4570190f', '49405788-6d5a-4736-b30a-59f77e6ed121','Arcfox', 'Arcfox', 'Arcfox');
+
+
+
+--Aro brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '1f0d9235-4e64-4e13-8d75-45655d73732e', '49405788-6d5a-4736-b30a-59f77e6ed121','Aro', 'Aro', 'Aro');
+
+
+
+--Ashok Leylan brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '913c17b3-0d35-4aa6-960b-306748f41e21', '49405788-6d5a-4736-b30a-59f77e6ed121','Ashok Leylan', 'Ashok Leylan', 'Ashok Leylan');
+
+
+
+--Asia brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'b7d27a4c-2f22-4ffc-9a4b-becc112090cf', '49405788-6d5a-4736-b30a-59f77e6ed121','Asia', 'Asia', 'Asia');
+
+
+
+--Aston Martin brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '537a70a8-54ce-4724-9811-512f59cf0207', '49405788-6d5a-4736-b30a-59f77e6ed121','Aston Martin', 'Aston Martin', 'Aston Martin');
+
+
+
+--Audi brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '3cca32fb-aeef-4671-a01c-4d2306778803', '49405788-6d5a-4736-b30a-59f77e6ed121','Audi', 'Audi', 'Audi');
+
+
+
+--Austin brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'daadb253-a32a-4249-9ea5-2940503b6e50', '49405788-6d5a-4736-b30a-59f77e6ed121','Austin', 'Austin', 'Austin');
+
