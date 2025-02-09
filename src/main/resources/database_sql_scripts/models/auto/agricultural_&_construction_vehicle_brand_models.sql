adeb1b69d54bf71ffbffc00e1742530025009ef5
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2025-02-09 13:00:35 +0500

    ...

---
+++ src/main/resources/database_sql_scripts/models/auto/agricultural_&_construction_vehicle_brand_models.sql
@@ -0,0 +1,112 @@
+--John Deere brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '60493abd-4573-4dd6-8e5e-59c84290ceec', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','John Deere', '', 'Джон Дир');
+
+
+
+--Caterpillar brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '96ce2e42-b72b-432f-bcd9-525d02bd616f', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','Caterpillar', '', 'Катерпиллар');
+
+
+
+--Komatsu brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '7f303e1c-98f6-40f8-8d65-5ecaeb9f1c45', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','', '', 'Комацу');
+
+
+
+--Liebherr brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'a69a95fb-b169-4550-9654-5b593e612b70', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','Liebherr', '', 'Либхерр');
+
+
+
+--JCB brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'b4d20bf8-8283-48a5-84ba-74ada188e02f', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','', '', 'Джей Си Би');
+
+
+
+--Case IH brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '3ea49884-b0c8-46ac-a26f-6eed4a151b7c', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','Case IH', '', 'Кейс IH');
+
+
+
+--New Holland brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'b5271f70-f239-41ef-9632-08b1f20d3bc7', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','New Holland', '', 'Нью Холланд');
+
+
+
+--Massey Ferguson brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '218c3015-3429-4e1a-8e0a-0ae6458ec20c', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','Massey Ferguson', '', 'Масси Фергюсон');
+
+
+
+--Fendt brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '4130bcd8-5a29-4d72-844d-e2b61403712a', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','', '', 'Фендт');
+
+
+
+--Claas brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '232417e4-5358-449c-8fd1-f96debe085a5', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','', '', 'Клаас');
+
+
+
+--Kubota brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'e073c524-077c-45e9-aa8c-05b0fb8d65e3', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','', '', 'Кубота');
+
+
+
+--Bobcat brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '07025d15-37a5-410c-a721-c5a97269cf66', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','Bobcat', '', 'Бобкэт');
+
+
+
+--Hitachi Construction Machinery brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '2495ec48-a244-4d15-8629-4a0bf330ec31', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','Hitachi Construction Machinery', '', 'Хитачи Строительные Машины');
+
+
+
+--Volvo Construction Equipment brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'd291a645-9591-48ce-88b3-213c412a2f1e', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','Volvo Construction Equipment', '', 'Вольво Строительное Оборудование');
+
+
+
+--Terex brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '9d0f7a9d-1c52-44a0-9a9b-6a63be6aa433', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','', '', 'Терекс');
+
+
+
+--Doosan brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '4ab127cf-838a-4d7c-a3d7-42d7a32cfc5a', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','Doosan', '', 'Дусан');
+
+
+
+--SANY brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '41f78363-909f-476c-89c6-8c2fbd3b9db1', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','', '', 'САНИ');
+
+
+
+--XCMG brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '1f204c2d-901e-429a-93f8-0b97d4c50459', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','', '', '');
+
+
+
+--Hyundai Construction Equipment brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'c28f7b36-a3e1-4b37-ac51-27616ae116e7', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','Hyundai Construction Equipment', '', 'Хёндэ Строительное Оборудование');
+
+
+
+--Manitou brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '3cab7ef9-d364-4853-911b-15df64d3e4b6', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','Manitou', '', 'Маниту');
+
+
+
+--Belarus MTZ brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'e9c84240-9a0d-4c93-9998-9a14fdc68c19', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','', '', 'Беларус МТЗ');
+
+
+
+--Challenger brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'cc70a983-b298-46fa-b53c-1cede2811f1e', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','', '', 'Челленджер');
+
+
+
+--Mahindra brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '5f1202a3-d3bc-4b10-a92f-98350764cd0d', '5e5c6c0e-7316-474f-b8fc-f31491e2acd2','', '', 'Махиндра');
---
+++ src/main/resources/database_sql_scripts/models/auto/special_vehicle_brand_models.sql
@@ -0,0 +1,82 @@
+--Caterpillar brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '7ecd201d-a037-43b1-8e6c-77ccbe464686', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','Caterpillar', '', 'Катерпиллар');
+
+
+
+--John Deere brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '0f7a7ee0-3c3a-4d2e-839b-51b9ae546ea2', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','John Deere', '', 'Джон Дир');
+
+
+
+--Komatsu brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '28e8f325-2303-45f9-9da2-0e2f583ad486', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','', '', 'Комацу');
+
+
+
+--Liebherr brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '69c0993a-ff30-44e0-b8ee-dfb1659987bc', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','Liebherr', '', 'Либхерр');
+
+
+
+--Bobcat brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '5bf5d240-b802-492c-b83e-8c6d0f8477c8', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','Bobcat', '', 'Бобкэт');
+
+
+
+--Hitachi brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '78875e78-7713-415c-a53f-5d0a8dd44b5f', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','Hitachi', '', 'Хитачи');
+
+
+
+--Volvo Construction Equipment brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'e2d037ec-459e-4e05-bd77-564755849882', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','Volvo Construction Equipment', '', 'Вольво Строительное Оборудование');
+
+
+
+--JCB brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '3605c392-6eaa-41eb-8bd4-456afe4dd791', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','', '', 'Джей Си Би');
+
+
+
+--Case Construction brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '56b5d438-3db1-4b0c-beb7-1fbf0650fa88', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','Case Construction', '', 'Кейс Строительство');
+
+
+
+--Terex brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '0c99e0df-08d9-4a5e-8e00-8df004bbe9f1', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','', '', 'Терекс');
+
+
+
+--Manitou brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '57197448-b346-4770-b111-a341561e3572', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','Manitou', '', 'Маниту');
+
+
+
+--Doosan brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '81e832bb-29ba-498c-9093-75ed16204449', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','Doosan', '', 'Дусан');
+
+
+
+--New Holland Construction brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'fa71dc08-4b40-425c-9cd7-99bf4b218618', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','New Holland Construction', '', 'Нью Холланд Строительство');
+
+
+
+--SANY brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'c90db9ae-9cc7-4388-9264-eb46a0e0fab8', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','', '', 'САНИ');
+
+
+
+--XCMG brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'e2d01e9d-fbe0-4875-a5db-c36dc09ed9d2', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','', '', '');
+
+
+
+--Hyundai Construction Equipment brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', 'ff701ff6-cc08-4d8a-bcf2-308d924c1206', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','Hyundai Construction Equipment', '', 'Хёндэ Строительное Оборудование');
+
+
+
+--Bell Equipment brand models
+INSERT INTO attribute_values (attribute_value_id, parent_id, attribute_id, value, value_uz, value_ru ) VALUES ('', '21f34d76-682f-4fe1-9c43-94c11b8983ee', 'fca31431-c2d6-468a-8e3b-693b7d0e9f85','Bell Equipment', '', 'Белл Оборудование');
