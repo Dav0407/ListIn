a35b6c59ee1f8fa44f01b36d045cec86caf6da8b
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2025-03-03 17:03:26 +0500

    ...

--- src/main/java/com/igriss/ListIn/location/dto/CountyResponseDTO.java
+++ src/main/java/com/igriss/ListIn/location/dto/CountyResponseDTO.java
@@ -1,18 +1,4 @@
 package com.igriss.ListIn.location.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class CountyResponseDTO {
-    private String valueEng;
-    private String valueUz;
-    private String valueRu;
 }
