205825f381b314e176ebf4d70d417ee1ec35cf75
Davronbek Normamatov <davronnormamatov004@gmail.com>
2025-03-06 17:16:55 +0500

    location logic changed

--- src/main/java/com/igriss/ListIn/publication/service_impl/PublicationServiceImpl.java
+++ src/main/java/com/igriss/ListIn/publication/service_impl/PublicationServiceImpl.java
@@ -39,6 +39,7 @@ import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
