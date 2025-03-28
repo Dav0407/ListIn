efa12744868d71bf8bbd29a3e39efe25bb2aee76
Abdulaxad7 <abdulaxadisroilov7@gmail.com>
2025-03-29 04:00:55 +0500

    clothes categories inserts

--- src/main/resources/database_sql_scripts/numerics/clothes_numeric_fields.sql
+++
@@ -1,5 +0,0 @@
---Women's clothe numeric fields
-INSERT INTO numeric_fields (field_id, category_id, field_name, field_name_uz, field_name_ru, description, description_uz, description_ru) VALUES ('4ec2ae53-53ee-4070-b8a4-634f250fa18e', '93e23e28-e0f8-427e-aa39-81a32d447285','Sleeve Length', 'Yeng uzunligi', 'Длина рукава','The length of the sleeve in centimeters (cm) or inches (in).','Yeng uzunligi santimetr (cm) yoki dyuym (in) da.','Длина рукава в сантиметрах (см) или дюймах (in).');
-
-
-
